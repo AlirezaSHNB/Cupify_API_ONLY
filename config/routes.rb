816aa19deca602132de3e0350404044cba4e1516
AlirezaSHNB <alirezasharifpour79@gmail.com>
2023-07-29 12:04:31 +0330

    add routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,35 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root 'home#index'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get 'users/new', to: 'users#new', as: :new_user
+  post 'users', to: 'users#create'
+  get '/users/dashboard', to: 'users#dashboard'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  post '/check_username_availability', to: 'users#check_username_availability'
+
+
+  resources :cups do
+    resources :leagues do
+      resources :matches do
+        resources :rounds do
+          resources :scores
+          resources :fouls
+        end
+      end
+    end
+    resources :knockouts do
+      resources :matches do
+        resources :rounds do
+          resources :scores
+          resources :fouls
+        end
+      end
+    end
+  end
+  resources :participants
+  resources :players do
+    get 'search_by_name', on: :collection
+  end
 end
